b4cabf8c2172cf6bc26172aa7573c21891ed1c68
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-04-29 22:08:52 +0530

    add more sections

--- backend/backend.py
+++ backend/backend.py
@@ -4,36 +4,73 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-url = "https://indianexpress.com/"
+# url = "https://indianexpress.com/"
 
-def scrape_news(url):
+def scarpe_news(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Identify news article elements using appropriate selectors (e.g., class names, IDs, tags)
-    news_articles = soup.find_all('div', class_='other-article')  # Replace with actual selectors
-
+    news_articles = soup.find_all('div', class_='other-article') 
     news_data = []
     for article in news_articles:
-        # Extract news data (headline, summary, URL) using proper selectors
-        # headline = article.find('h3').text.strip()  # Replace with actual selectors
-        summary = article.find('h3').text.strip()  # Replace with actual selectors
-        image_url= article.find('img').get('src')
+        summary = article.find('h3').text.strip()
+        image_url = article.find('img').get('src')
+        # image_alt = article.find('img').get('alt')
         # article_url = article.find('a')['href']
-
         news_data.append({
             # 'headline': headline,
             'summary': summary,
             'image_url': image_url,
+            # 'image_alt': image_alt,
             # 'url': article_url
         })
-
     return news_data
 
-news = scrape_news(url)
-print(news)
+def scarper(url,div_name=None):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    news_articles = soup.find_all('div', class_=div_name) 
+    news_data = []
+    for article in news_articles:
+        headline = article.find(class_="title").text.strip()
+        image_url = article.find('img').get('src')
+        summary = article.find('p').text.strip()
+        news_data.append({
+            'headline': headline,
+            'summary': summary,
+            'image_url': image_url,
+        })
+    return news_data
+    
+news_top = scarpe_news("https://indianexpress.com/")
+news_business = scarper("https://indianexpress.com/section/business/")
+news_sports = scarper("https://indianexpress.com/section/sports/",div_name="nation")
+news_entertainment = scarper("https://indianexpress.com/section/entertainment/",div_name="stories")
+news_politics = scarper("https://indianexpress.com/section/political-pulse/")
+
 
 @app.get("/news/top")
 async def get_news():
-    # scraped_news = scrape_news("https://www.example.com/news")  # Replace with target URL
-    return news
+    return news_top
+
+
+@app.get("/news/business")
+async def get_news():
+    return news_business
+
+
+@app.get("/news/sports")
+async def get_news():
+    return news_sports
+
+@app.get("/news/business")
+async def get_news():
+    return news_business
+
+@app.get("/news/entertainment")
+async def get_news():
+    return news_entertainment
+
+@app.get("/news/politics")
+async def get_news():
+    return news_politics
+
